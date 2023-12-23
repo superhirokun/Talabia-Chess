cc0a26c71dd155eff9d821e0225b7ce97fdd4004
Yap Choo Kath Moon <92213060+superhirokun@users.noreply.github.com>
2023-12-23 14:02:48 +0800

    model/ChessPiece.java 	   model/TimePiece.java    model/gameboard.java

---
+++ model/ChessPiece.java
@@ -0,0 +1,9 @@
+package model;
+
+public abstract class ChessPiece{
+    private String[] Color = {"Black" , "Yellow"};
+    private boolean Captured;
+    private boolean Switch;
+
+    
+}
--- model/gameboard.java
+++ model/gameboard.java
@@ -1,5 +1,19 @@
 package model;
 
-public class gameboard{
+public class GameBoard{
+    private ChessPiece[][] board;
+
+    public GameBoard(){
+        this.board = new ChessPiece[7][6];
+    }
+
+    public void placePiece(ChessPiece piece, int x, int y){
+        this.board[x][y] = piece;
+    }
+
+    public void removePiece(int x, int y){
+        this.board[x][y] = null;
+    }
+
 
 }
