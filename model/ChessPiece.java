19adce8d4c412847f2707b29c6d9c2c660a27543
Anglified <trueanglified@gmail.com>
2023-12-26 00:50:34 +0800

    Finish adding the pieces classes

--- model/ChessPiece.java
+++ model/ChessPiece.java
@@ -1,9 +1,28 @@
 package model;
 
-public abstract class ChessPiece{
-    private String[] Color = {"Black" , "Yellow"};
-    private boolean Captured;
-    private boolean Switch;
+public abstract class ChessPiece {
+    private String row;
+    private String column;
+    private String[] colour;
+    private int turn;
+    private boolean pieceSwitch;
+    private boolean captured;
 
-    
+    public ChessPiece(String row, String column, String[] colour, int turn, boolean pieceSwitch) {
+        this.row = row;
+        this.column = column;
+        this.colour = colour;
+        this.turn = turn;
+        this.pieceSwitch = pieceSwitch;
+        this.captured = false;
+    }
+
+    public void pieceCount() {
+        // Include logic for pieceCount, aka check how many pieces are there on the board left
+        int pieceCount;
+    }
+
+    public void pieceSwitch() {
+        // Include logic for pieceSwitch, basically switch time and plus every 2 rounds
+    }
 }
