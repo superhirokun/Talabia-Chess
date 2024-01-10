7f6ca1bd63be21516a23ced3859cda055107492f
Yap Choo Kath Moon <92213060+superhirokun@users.noreply.github.com>
2024-01-11 00:30:27 +0800

    model/ArrowPiece    model/ChessPiece.java

--- model/ChessPiece.java
+++ model/ChessPiece.java
@@ -3,10 +3,11 @@ package model;
 public abstract class ChessPiece {
     private String row;
     private String column;
-    private String[] colour;
+    private String[] colour = {"black","yellow"};
     private int turn;
     private boolean pieceSwitch;
     private boolean captured;
+    private String position;
 
     public ChessPiece(String row, String column, String[] colour, int turn, boolean pieceSwitch) {
         this.row = row;
@@ -15,6 +16,10 @@ public abstract class ChessPiece {
         this.turn = turn;
         this.pieceSwitch = pieceSwitch;
         this.captured = false;
+    }
+
+    public String getColour(Integer colo){
+        return this.colour[colo];
     }
 
     public void pieceCount() {
