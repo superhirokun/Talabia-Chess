1ba91befffa9b925e0c4715b5edf58ab86bd40c0
Yap Choo Kath Moon <92213060+superhirokun@users.noreply.github.com>
2024-01-10 16:39:23 +0800

    model/GameBoard.java

--- model/GameBoard.java
+++ model/GameBoard.java
@@ -7,12 +7,35 @@ public class GameBoard{
         this.board = new ChessPiece[7][6];
     }
 
+    //Setter
+    public ChessPiece setPiece(ChessPiece piece, int x, int y){
+        return this.board[x][y] = piece;
+    }
+
+    public void setBoard(ChessPiece[][] board){
+        this.board = board;
+    }
+
+    //Getter
+    public ChessPiece getPiece(int x, int y){
+        return this.board[x][y];
+    }
+
+    public ChessPiece[][] getBoard(){
+        return this.board;
+    }
+
+    //Place and remove piece
     public void placePiece(ChessPiece piece, int x, int y){
         this.board[x][y] = piece;
     }
 
     public void removePiece(int x, int y){
         this.board[x][y] = null;
+    }
+
+    public void reset(){
+        //Reset the board
     }
 
 
