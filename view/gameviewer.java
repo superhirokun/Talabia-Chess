98385f409ce4bf912ee539dd996c58e850eaefdb
Hannan <1211104293@student.mmu.edu.my>
2024-01-24 09:19:12 +0800

    really basic stuff and a placeholder image

--- view/gameviewer.java
+++ view/gameviewer.java
@@ -1,1 +1,46 @@
 package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class gameviewer {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() ->{
+            JFrame frame = new JFrame("Tabalia chess thing");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLayout(new GridLayout(7,7));
+
+            for (int i = 0; i < 7; i++) {
+                for (int index = 0; index < 7; index++) {
+                    JButton button = new JButton();
+                    button.addActionListener(new ButtonClickListener());
+                    ImageIcon icon = new ImageIcon("view/pawns.jpg");
+                    button.setIcon(icon);
+
+                    frame.add(button);
+                }
+            }
+
+            frame.pack();
+            frame.setVisible(true);
+        });
+        
+
+    }
+
+
+    public static class ButtonClickListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            JButton sourceButton = (JButton) e.getSource();
+            sourceButton.setSelected(!sourceButton.isSelected());
+            if (sourceButton.isSelected()) {
+                sourceButton.setBackground(Color.BLUE);
+            } else {
+                sourceButton.setBackground(null);
+            }
+        }
+    }
+    
+}
