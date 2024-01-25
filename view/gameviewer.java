aec5b6760cbd21a7a239c2a48fd727d0eb0ad38f
Hannan <1211104293@student.mmu.edu.my>
2024-01-25 16:03:33 +0800

    added some color

--- view/gameviewer.java
+++ view/gameviewer.java
@@ -17,24 +17,33 @@ public class gameviewer{
         GameBoard gameBoard = new GameBoard(new GameBoard.BobTheBuilder());
         gameBoard.zaStarter(new BobTheBuilder());
         HashMap<Integer, ChessPiece> piecePositions = gameBoard.getPiecePosition();
-            System.out.println(piecePositions);      
+            System.out.println(piecePositions); 
+        Color boardColor1 = new Color(231, 237, 159);
+        Color boardColor2 = new Color(219, 164, 86); 
         SwingUtilities.invokeLater(() ->{
             JFrame frame = new JFrame("Tabalia chess thing");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLayout(new GridLayout(6,7));
             String piece;
             for (int i = 0; i < 42; i++) {
+                JButton button;
                 if (piecePositions.containsKey(i) && piecePositions.get(i) != null) {
                     piece = piecePositions.get(i).toString();
 
-                    JButton button = new JButton(piece);
+                    button = new JButton(piece);
                     button.addActionListener(new ButtonClickListener());
                     frame.add(button);
                 }
                 else{
-                    JButton button = new JButton();
+                    button = new JButton();
                     button.addActionListener(new ButtonClickListener());
                     frame.add(button);
+                }
+                if (i % 2 == 0) {
+                    button.setBackground(boardColor1);
+                }
+                else{
+                    button.setBackground(boardColor2);
                 }
             }
 
@@ -47,13 +56,15 @@ public class gameviewer{
 
 
     public static class ButtonClickListener implements ActionListener{
+        private Color previousColor;
         public void actionPerformed(ActionEvent e){
             JButton sourceButton = (JButton) e.getSource();
             sourceButton.setSelected(!sourceButton.isSelected());
             if (sourceButton.isSelected()) {
+                previousColor = sourceButton.getBackground();
                 sourceButton.setBackground(Color.BLUE);
             } else {
-                sourceButton.setBackground(null);
+                sourceButton.setBackground(previousColor);
             }
         }
     }
