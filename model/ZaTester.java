30bfab15cccf4161a070a91ca25dd46bf194400b
galaktose <89487521+galaktose@users.noreply.github.com>
2024-01-26 19:54:07 +0800

    Co-authored-by: Syahmi45 <Syahmi45@users.noreply.github.com> progress on game flow

--- model/ZaTester.java
+++ model/ZaTester.java
@@ -1,18 +1,42 @@
 package model;
-
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 import control.*;
-
 import model.GameBoard.BobTheBuilder;
-public class ZaTester {
-    protected static final String StartingFEN = "lhtsthl/ppppppp/7/7/PPPPPPP/LHTSTHL y 1";
+public class ZaTester{
     public static void main(String[] args) {
         GameBoard gameBoard = new GameBoard(new GameBoard.BobTheBuilder());
         BobTheBuilder builder = new BobTheBuilder();
         gameBoard.zaStarter(builder);
-        System.out.println(Arrays.toString(gamecontroller.zaFENDecoder(StartingFEN)));
-        System.out.println(builder.piecePosition);
-        System.out.println(gameBoard.getAllPiecePosition());
+        int turnBruh = 0;
+
+        System.out.println(Arrays.toString(gamecontroller.zaFENDecoder(GameBoard.StartingFEN)));
+        
+        
+        while (!gameBoard.isSunPieceCaptured()) {
+            
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter ur move ig : ");
+            //Integer test = scanner.nextInt();
+            //ChessPiece test1 = builder.piecePosition.get(test);
+            // user select a piece 
+            
+            gamecontroller.makeMoveBoardLogic(null, 0, gameBoard);
+
+            BoardLogic.turnCounter(turnBruh); //increments counter 
+            BoardLogic.zaSwitcher(); //check if next turn requires switching between plus and time piece
+            
+        }
+        
     }
+
+    
 }
+
+
+//GameBoard gameBoard = new GameBoard(new GameBoard.BobTheBuilder());
+//        BobTheBuilder builder = new BobTheBuilder();
+//        gameBoard.zaStarter(builder);
+//        System.out.println(Arrays.toString(gamecontroller.zaFENDecoder(StartingFEN)));
+//        System.out.println(builder.piecePosition);
+//        System.out.println(gameBoard.getAllPiecePosition());
