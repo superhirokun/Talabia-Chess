e35761964cc50eb392649fb281dc4a9574ef28a4
galaktose <89487521+galaktose@users.noreply.github.com>
2024-01-26 19:55:04 +0800

    minor edits

--- model/BoardLogic.java
+++ model/BoardLogic.java
@@ -70,65 +70,7 @@ public class BoardLogic {
 
 
     /*Should work further testing neeeded*/
-    public static GameBoard makeMove(ChessPiece piece, int destination, GameBoard gamer){   //make the move
-        BobTheBuilder builder = new BobTheBuilder();
-        int initialPosition = piece.getPosition();
-        HashMap<Integer, ChessPiece> prevPiecePosition = new HashMap<>();
-        prevPiecePosition = gamer.getAllThePiece();  //get the previous position of the piece
-        for(int i = 0; i < totalSquare; i++){
-           if(prevPiecePosition.get(i) != null){    //check if the piece is not null
-               if(prevPiecePosition.get(i).getPosition() == initialPosition){
-                   prevPiecePosition.get(i).setPosition(destination);   //set the position of the piece to the new position which is the destination
-                   builder.placePiece(gamer, prevPiecePosition.get(i), destination);
-               }else if(prevPiecePosition.get(i).getPosition() == destination && prevPiecePosition.get(i).getCaptured() == true){   //check if the piece is captured
-                   continue;
-               }else if(canSwitch == true){     //switch the time piece with the plus piece when the turn is even
-                    if(prevPiecePosition.get(i) == gamer.plusPiece.get(i)){
-                        builder.placePiece(gamer, new TimePiece(gamer.plusPiece.get(i).getColor(), i), i);
-                    }else if(prevPiecePosition.get(i) == gamer.timePiece.get(i)){
-                        builder.placePiece(gamer, new PlusPiece(gamer.timePiece.get(i).getColor(), i), i);
-                    }
-               }
-               else{
-                   builder.placePiece(gamer, prevPiecePosition.get(i), i);   //if the piece is not the piece that is moving then place the piece at the same position
-               }
-            }else{
-                builder.placePiece(gamer, null, i);    //if the piece is null then place a null piece
-            }
-        }
-        
-        return builder.build();
-    }
 
-    /**
-     * Decodes the given zaFEN string and returns an array of board pieces.
-     * 
-     * @param zaFEN the zaFEN string to be decoded
-     * @return an array of board pieces representing the decoded zaFEN string
-     */
-    public static String[] zaFENDecoder(String zaFEN){
-        String zaFENString = zaFEN.split(" ")[0];
-        int zalength = zaFENString.length();
-        String[] zaBoardPiece = new String[BoardLogic.totalSquare];
-        int zaCounter = 0;
-        for(int i = 0; i < zalength; i++){
-            char zaCurrentChar = zaFENString.charAt(i);
-            if(zaCurrentChar == '/'){
-                continue;
-            }
-            if(Character.isDigit(zaCurrentChar)){
-                for(int j = 0; j < Character.getNumericValue(zaCurrentChar); j++){
-                    zaBoardPiece[zaCounter] = "-";
-                    zaCounter++;
-                }
-                
-            }else{
-                zaBoardPiece[zaCounter] = String.valueOf(zaCurrentChar);
-                zaCounter++;
-            }
-        }
-        return zaBoardPiece;   
-    }
 
     /**
      * Decodes the color from the given FEN string.
